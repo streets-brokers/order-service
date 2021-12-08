57565140157002be786d6d03c4e219c0af902286
Okai Yeboah <okai.yeboah@turntabl.io>
2021-12-08 15:55:57 +0000

    fix merge conflict

--- src/main/java/com/streets/ordersvc/OrderServiceApplication.java
+++ src/main/java/com/streets/ordersvc/OrderServiceApplication.java
@@ -2,20 +2,15 @@ package com.streets.ordersvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
-=======
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@SpringBootApplication
 @EnableScheduling
->>>>>>> 95d4f7e4cc5c2bef772e0d9c9a7feeeb1a610c81
 public class OrderServiceApplication {
 
     @Bean
